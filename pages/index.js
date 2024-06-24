6f08c42a1870d99657846892c07ab3af7cbe9136
Kumaraguru <19eucs071@skcet.ac.in>
2024-06-24 17:05:41 +0530

    fixing hydration error

--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,91 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card"
 import { Github } from "lucide-react";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import IdeaCard from "@/components/ui/idea-card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className=" bg-black min-h-screen">
-      <div className="bg-[#0d0d0d]">
-        <div className="flex justify-between p-10 items-center">
-          <img className=" inline-flex justify-center w-40" alt="Web3PrivacyNow" src="https://web3privacy.info/logo.svg" />
-          <div className="items-center flex space-x-10">
-            <button className="archivo opacity-40 text-sm">CONTRIBUTORS</button>
-            <button className="border p-2 archivo text-xs flex space-x-3 items-center">
-              <h3>
-                CONTRIBUTE AN IDEA
-              </h3>
-              <Github className="w-5" />
-            </button>
-          </div>
-        </div>
-        <h1 className="major text-4xl text-center mt-10">privacy idea generator</h1>
-        <h4 className="archivo text-center mt-5 opacity-40">Generate amazing privacy focussed project ideas curated by Web3PrivacyNow community</h4>
-        <div className="flex justify-center mt-20 pb-20">
-          <button className="bg-white text-black px-5 p-2 text-xs archivo flex space-x-3 items-center">GENERATE AN IDEA
-          </button>
+
+    <main className=" bg-black w-full min-h-screen pb-10">
+      <nav className="flex justify-between p-5 items-center bg-[#0d0d0d]">
+        <img className=" inline-flex justify-center w-40" alt="Web3PrivacyNow" src="https://web3privacy.info/logo.svg" />
+        <button className="border archivo text-xs flex space-x-3 items-center p-2">
+          <h3>
+            CONTRIBUTE AN IDEA
+          </h3>
+          <Github className="w-4" />
+        </button>
+      </nav>
+      <div className="bg-[#0d0d0d] h-96 pt-20">
+        <h1 className="major text-center text-3xl">privacy idea generator</h1>
+        <h3 className="archivo text-center text-[#4c4c4c] mt-5 px-5">Generate ideas curated by the Web3Privacy Now community and experts</h3>
+        <div className="flex justify-center mt-10 md:mt-20">
+          <Dialog>
+            <DialogTrigger>
+              <div className="bg-white text-[#0d0d0d] archivo text-sm flex space-x-3 items-center p-2">
+                <h3>
+                  GENERATE
+                </h3>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogDescription className="">
+                <div className="h-10">
+      <h1 className="archivo text-lg mb-4">zk loyalty</h1>
+    </div>
+    <hr className="opacity-10 my-2" />
+    <div className=" h-20">
+      <h3 className="archivo mt-4 text-sm opacity-50">A GDPR compliant, anonymous loyalty program	</h3>
+    </div>
+    <div className='flex justify-between items-center my-3'>
+    <div className="flex space-x-3 w-2/3">
+      <Badge variant="secondary">Tag 1</Badge>
+      <Badge variant="secondary">Tag 2</Badge>
+      <Badge variant="secondary">Tag 3</Badge>
+    </div>
+    <div className='w-1/3 text-xs text-end flex justify-end'>
+       <Github className='w-5 h-5' />
+    </div>
+    </div>
+    <hr className="opacity-10 my-3" />
+    <h3 className="archivo mt-2 text-sm opacity-50 text-end hover:underline cursor-pointer">Source : @hackyguru</h3>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
-      <div className="bg-black w-full p-20">
-        <h1 className="major text-2xl">featured ideas</h1>
-        <div className="flex justify-between space-x-5 mt-10">
-          <div className=" bg-[#0d0d0d] w-1/3 p-6">
-            <div className="h-16">
-              <h1 className="archivo text-lg mb-4">zk loyalty</h1>
-            </div>
-            <hr className="opacity-10 my-2" />
-            <div className=" h-32">
-              <h3 className="archivo mt-4 text-sm opacity-50">A GDPR compliant, anonymous loyalty program	</h3>
-            </div>
-            <div className="flex space-x-3 mt-3">
-              <Badge variant="secondary">Tag 1</Badge>
-              <Badge variant="secondary">Tag 2</Badge>
-              <Badge variant="secondary">Tag 3</Badge>
-            </div>
+      <div className="mt-10">
+        <h1 className="major text-center text-xl">featured ideas</h1>
+        <Tabs defaultValue="account" className="mt-5">
+          <div className="flex justify-center w-full">
+            <TabsList className="archivo">
+              <TabsTrigger value="account">Community ideas</TabsTrigger>
+              <TabsTrigger value="password">Expert ideas</TabsTrigger>
+            </TabsList>
           </div>
-
-          <div className=" bg-[#0d0d0d] w-1/3 p-6">
-            <div className="h-16">
-              <h1 className="archivo text-lg mb-4">Whisper</h1>
-            </div>
-            <hr className="opacity-10 my-2" />
-            <div className=" h-32">
-              <h3 className="archivo mt-4 text-sm opacity-50">Platform for anonymized members to create privacy-preserved groups	</h3>
-            </div>
-            <div className="flex space-x-3 mt-3">
-              <Badge variant="secondary">Tag 1</Badge>
-              <Badge variant="secondary">Tag 2</Badge>
-              <Badge variant="secondary">Tag 3</Badge>
-            </div>
+          <div className="mt-10 w-full text-lef">
+            <TabsContent value="account">
+              <div className="md:flex block space-y-5 md:space-y-0 w-full justify-between px-5 md:space-x-5">
+                <IdeaCard />
+                <IdeaCard />
+                <IdeaCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="password">Change your password here.</TabsContent>
           </div>
-
-          <div className=" bg-[#0d0d0d] w-1/3 p-6">
-            <div className="h-16">
-              <h1 className="archivo text-lg mb-4">ZeroDock</h1>
-            </div>
-            <hr className="opacity-10 my-2" />
-            <div className=" h-32">
-              <h3 className="archivo mt-4 text-sm opacity-50">Providing provable off-chain computation based on on-chain events	</h3>
-            </div>
-            <div className="flex space-x-3 mt-3">
-              <Badge variant="secondary">Tag 1</Badge>
-              <Badge variant="secondary">Tag 2</Badge>
-              <Badge variant="secondary">Tag 3</Badge>
-            </div>
-          </div>
-        </div>
+        </Tabs>
       </div>
     </main>
   );
